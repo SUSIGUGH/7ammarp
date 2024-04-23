a174111f23a7f36690f231811aecd5e36b0fba8f
susigugh <susigugh@gmail.com>
2024-04-23 01:27:47 +0000

    Added terraform folder

---
+++ terraform/main.tf
@@ -0,0 +1,13 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.0"
+    }
+  }
+}
+
+# Define the provider
+provider "aws" {
+  region = "ap-south-1"
+}
